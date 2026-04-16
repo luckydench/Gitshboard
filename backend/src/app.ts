20658cb9666fa99ade9b2a962e84170bf97c7751
Jung TaeSeung <mike415415@naver.com>
2026-04-16 12:48:30 +0900

    feat : 기본 익스프레스 서버 개설 코드 추가

---
+++ backend/src/app.ts
@@ -0,0 +1,25 @@
+import express from  'express';
+import type {Request, Response} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+// 기본 라우트, 서버를 헬스 체크 합니다.
+app.get('/', (req: Request, res: Response) => {
+  res.send('🚀 Express + TypeScript 서버 실행 중');
+});
+
+
+// 서버 실행
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
