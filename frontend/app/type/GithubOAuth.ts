cd2a55c7865ef87e7d775b28773353cfe0f7d780
Jung TaeSeung <mike415415@naver.com>
2026-04-27 19:43:50 +0900

    feat : oauth github 추가

---
+++ frontend/app/type/GithubOAuth.ts
@@ -0,0 +1,24 @@
+/**
+ *  Github oauth 공식 규격 파라미터
+ */
+export interface GithubOAuthParams {
+  client_id: string;
+  redirect_uri: string;
+  scope?: string;
+  state? : string;
+}
+
+/**
+ * Github oauth scope 제한 파라미터
+ * 프로젝트 내에선 GithubScope 타입으로 제한하여 사용하도록 권장합니다.
+ */
+export interface StrictGithubOAuthParams  
+extends Omit<GithubOAuthParams, 'state'>,  Record<string, string> {
+    scope : GitHubScope;
+}
+// strict 타입이기 때문에 Record로 undefined가 불가능하게 강제시킴
+// state는 CSRF 공격 방지용으로 사용되지만, 현재 구현에서는 생략시켰음. 필요에 따라 추가할 수 있음
+
+
+// github oauth scope 타입
+export type GitHubScope = 'user' | 'repo' | 'gist' | 'read:org';
