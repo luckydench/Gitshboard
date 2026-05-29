42b2b7610c46c1336a10e5ae1b78a64c292965dc
Jung TaeSeung <mike415415@naver.com>
2026-05-29 18:23:01 +0900

    feat : frontend GraphQLComponent 추가

--- frontend/app/routes/testpage.tsx
+++ frontend/app/routes/testpage.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useOutletContext } from "react-router";
 import type { FloatState } from "~/components/layout/DashboardLayout";
 import DenchComponent from "~/components/testpage/DenchComponent";
+import GraphQLComponent from "~/components/testpage/GraphQLComponent";
 import TanstackComponent from "~/components/testpage/TanstackComponent";
 
 export interface TestResponse{
@@ -27,7 +28,7 @@ export default function TestPage(){
             <h1>Test Page</h1>
             {floatMode == "1" && <DenchComponent/>}
             {floatMode == "2" && <TanstackComponent/>}
-
+            {floatMode == "3" && <GraphQLComponent/>}
         </>
     )
 }
